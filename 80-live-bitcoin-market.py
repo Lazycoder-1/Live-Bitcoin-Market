13ef8bce85c77563381c4760a9f80d13fb5df9d0
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-07-11 17:31:45 +0000

    Add files via upload
    Added the code that outputs the current value of bitcoin to be updated every 5 seconds.

--- 80-live-bitcoin-market.py
+++ 80-live-bitcoin-market.py
@@ -1,6 +1,5 @@
 # api = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
-
-
+import time
 import requests
 
 # Replace 'YOUR_API_KEY' with your actual API key from CoinMarketCap
@@ -13,17 +12,17 @@ headers = {
     'X-CMC_PRO_API_KEY': API_KEY,
 }
 
-# Make a request to the API
 res = requests.get(URL, headers=headers)
-
-# Convert the response to JSON
 data = res.json()
 
-# Print the data
-print(data)
-
-
-
-
-
+# Extract Bitcoin data
+while 1 == 1:
+    API_KEY = '97655b6a-ee07-4105-82a4-b82976527a2b'
+    URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    for cryptocurrency in data['data']:
+        if cryptocurrency['symbol'] == 'BTC':
+            bitcoin_price = cryptocurrency['quote']['USD']['price']
+            print(f"The current value of Bitcoin is: ${bitcoin_price:.2f}")
+            time.sleep(5)
+            break
 
